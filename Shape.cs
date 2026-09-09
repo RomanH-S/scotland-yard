318d87e1d1c645e7513b9ae5fd6e3d575092ee1d
internets <pete@tankfarm.io>
2026-09-09 10:08:23 -0400

    Refactor Shape#IrregularPolygon

--- Shape.cs
+++ Shape.cs
@@ -4,7 +4,6 @@ using Godot;
 public partial class Shape : Node2D
 {
     private Line2D _line;
-    
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -35,49 +34,31 @@ public partial class Shape : Node2D
 
     public void IrregularPolygon(int sides, Vector2 position, int radius, int angleVariance)
     {
-
         var random = new Random();
-        // varies + or - 30 degrees right now
-        //int angleVariance = 24;
-        double angle = 0;
+        double angle;
         double x = 0;
         double y = 0;
-        _line = new Line2D();
-        _line.Width = 2.0f;
-        _line.DefaultColor = Colors.Red;
-        double totalGeneratingAngleSum = 0;
+        _line = new Line2D { Width = 2.0f, DefaultColor = Colors.Red };
 
         // higher value for angle variance makes it vary less
         double randomAngle = Math.PI * 2 / random.Next(-angleVariance, angleVariance);
         double firstAngle = randomAngle;
 
-        for (int a = 0; a <= sides; a++)
-            {
-                totalGeneratingAngleSum = totalGeneratingAngleSum + a*(2*Math.PI / sides);
-            }
-        for (int i = 0; i < sides ; i++)
+        for (int i = 0; i < sides; i++)
         {
-            //angle = 2*Math.PI / random.Next(-angleVariance, angleVariance) + angle + i*(2*Math.PI / sides);
-            angle = i*(2*Math.PI / sides) + randomAngle;
-            
-            //GD.Print($"{totalGeneratingAngleSum}");
-            x = (double)(Math.Cos(angle) * radius) + x;
-            y = (double)(Math.Sin(angle) * radius) + y;
-            _line.AddPoint(new Vector2((float)x, (float)y) + position + new Vector2(200,200));
-            //totalGeneratingAngleSum = Math.PI / 2/*totalGeneratingAngleSum - angle*/;
-
+            angle = i * (2 * Math.PI / sides) + randomAngle;
+            AddPoint(angle, radius, position + new Vector2((float)x, (float)y));
             randomAngle = Math.PI * 2 / random.Next(-angleVariance, angleVariance);
-
         }
 
-        x = (double)(Math.Cos(firstAngle) * radius);
-        y = (double)(Math.Sin(firstAngle) * radius);
-        //x = Math.Cos(totalGeneratingAngleSum - angle) * radius;
-        //y = Math.Sin(totalGeneratingAngleSum - angle) * radius;
-        _line.AddPoint(new Vector2((float)x, (float)y) + position + new Vector2(200,200));
+        AddPoint(firstAngle, radius, position);
         AddChild(_line);
+    }
 
-        //GD.Print($"{angle}");
-        GD.Print($"{totalGeneratingAngleSum}");
+    private void AddPoint(double angle, int radius, Vector2 position)
+    {
+        double x = (double)(Math.Cos(angle) * radius);
+        double y = (double)(Math.Sin(angle) * radius);
+        _line.AddPoint(new Vector2((float)x, (float)y) + position + new Vector2(200, 200));
     }
 }
